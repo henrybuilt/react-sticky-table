8764ff00f061187ad9099ec902243bc6099cec63
Max Hudson <max@henrybuilt.com>
2016-12-09 18:19:37 -0800

    Table showing

---
+++ src/Demo/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+import { StickyTable, Row, Cell } from 'react-sticky-table';
+import 'react-sticky-table/dist/react-sticky-table.css';
+
+export default class Demo extends Component {
+  render() {
+    var rows = [];
+    var cells;
+
+    for (var r = 0; r < 50; r++) {
+      cells = [];
+
+      for (var c = 0; c < 20; c++) {
+        cells.push(<Cell key={c}>{(r === 0 ? 'header ' : 'cell ') + c}</Cell>);
+      }
+
+      rows.push(<Row key={r}>{cells}</Row>);
+    }
+
+    return (
+      <div style={{width: '500px', height: '300px'}}>
+        <StickyTable>
+          {rows}
+        </StickyTable>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Demo from './Demo';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Demo />,
   document.getElementById('root')
 );
