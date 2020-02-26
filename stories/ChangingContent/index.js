4ac27821b79caa96baa1350d407f8fadbd772a02
maxhudson <max@henrybuilt.com>
2020-02-26 10:17:04 -0800

    cleaned up eslint

--- stories/ChangingContent/index.js
+++ stories/ChangingContent/index.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import { StickyTable, Row, Cell } from "../../src";
+import { StickyTable, Row, Cell } from '../../src';
 
-import Button from "../Button";
+import Button from '../Button';
 
 export default class ChangingContent extends Component {
   constructor(props) {
@@ -15,7 +15,7 @@ export default class ChangingContent extends Component {
       cells = [];
 
       for (var c = 0; c < 20; c++) {
-        cells.push(<Cell key={c}>{(r === 0 ? "Header " : "Cell ") + c}</Cell>);
+        cells.push(<Cell key={c}>{(r === 0 ? 'Header ' : 'Cell ') + c}</Cell>);
       }
 
       rows.push(<Row key={r}>{cells}</Row>);
@@ -35,7 +35,7 @@ export default class ChangingContent extends Component {
 
       for (var c = 0; c < 20; c++) {
         cells.push(
-          <Cell key={c}>{(r === 0 ? "New Header " : "Cell ") + c}</Cell>
+          <Cell key={c}>{(r === 0 ? 'New Header ' : 'Cell ') + c}</Cell>
         );
       }
 
@@ -51,7 +51,7 @@ export default class ChangingContent extends Component {
         <Button onClick={this.changeContent.bind(this)}>Change content</Button>
         <br />
         <br />
-        <div style={{ width: "100%", height: "200px" }}>
+        <div style={{ width: '100%', height: '200px' }}>
           <StickyTable stickyColumnCount={0}>{this.state.rows}</StickyTable>
         </div>
       </div>
